8bb5ada50fd6cf0bec0400b136f0888f5d254d92
kumaripawan <157499058+kumaripawan@users.noreply.github.com>
2024-02-26 17:16:02 +0000

    Update universe.py

--- universe.py
+++ universe.py
@@ -13,3 +13,22 @@ class Universe:
     def display(self) -> None:
         for planet in self.__planets:
             print(planet)
+            from typing import List
+from planet import Planet
+
+class Universe:
+
+    def __init__(self):
+        self.__planets = []
+
+    def generate(self, planet_names:List[str]) -> None:
+        generated_planets = []
+
+        for planet_name in planet_names:
+            planet = Planet(planet_name)
+            self.__planets.append(planet)
+            generated_planets.append(planet)
+
+    def display(self) -> None:
+        for planet in self.__planets:
+            print(f"Planet: {planet.__name}, Population: {planet.population()}")
