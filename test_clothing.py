483d142f222b51d9991eb8ac0dc8b1dd9b800a4d
Pawan kumari <2p44@solent.ac.uk>
2024-02-26 17:12:13 +0000

    Added classes to represent clothing.

---
+++ test_clothing.py
@@ -0,0 +1,10 @@
+import unittest
+from clothing import Clothing
+
+class TestClothing(unittest.TestCase):
+
+    def test_create_clothing(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
