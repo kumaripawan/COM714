8aa54143b70f8e8349352a8cfeea7f7096970b75
Pawan kumari <2p44@solent.ac.uk>
2024-02-26 16:26:59 +0000

    Created a basic human class

---
+++ firstproject.py
@@ -0,0 +1,20 @@
+print("Who broke my heart?")
+user_response = input()
+print("My first love, " + user_response + ", broke my heart for the first time.")
+print("And I was like baby, baby, baby oh.")
+# Display the initial message
+print("Who sees me rolling? (cops, wardens, other)")
+
+# Read user's response
+user_response = input()
+
+# Check user's response and display the corresponding message
+if user_response.lower() == "cops":
+    print("They see me rolling. They hating. They hoping they gonna catch me riding dirty!")
+elif user_response.lower() == "wardens":
+    print("My music's so loud. I'm swanging. They hoping they gonna catch me riding dirty!")
+else:
+    print("Trying to catch me riding dirty!")
+
+
+
