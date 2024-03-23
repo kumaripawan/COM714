a22151a9512c06488c54c7faa359bbb1e12894f0
kumaripawan <157499058+kumaripawan@users.noreply.github.com>
2024-03-23 17:29:54 +0000

    Add files via upload

---
+++ TestSuperHuman.py
@@ -0,0 +1,11 @@
+import unittest
+from super_human import SuperHuman
+
+class TestSuperHuman(unittest.TestCase):
+
+    def test_fly(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
