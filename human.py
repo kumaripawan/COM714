9d2f3378a59d198624c1696970b189dfbc32aa45
kumaripawan <157499058+kumaripawan@users.noreply.github.com>
2024-03-23 12:39:55 +0000

    Update human.py

--- human.py
+++ human.py
@@ -99,6 +99,27 @@ class Human:
             return True
         else:
             return False
+        
+            def undress(self, clothing: Clothing):
+                self.clothing.remove(clothing)
+                from animal import Animal
+                from clothing import Clothing
+
+                class Human(Animal):
+                    MOVE_ENERGY = 10
+                    REPRODUCE_ENERGY = 20
+
+                    def __init__(self, name: str, age: int = 0, energy: int = Animal.MAX_ENERGY) -> None:
+                        super(Human, self).__init__(name, age, energy)
+                        self.clothing = []
+
+                    def dress(self, clothing: Clothing):
+                        self.clothing.add(clothing)
+
+                    def undress(self, clothing: Clothing):
+                        self.clothing.remove(clothing)
+
+
 
 
 
